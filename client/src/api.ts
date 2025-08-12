908a2c11eb6919c197e98a61cda1ed78b7ebfef8
hilal <hilal.hidile@gmail.com>
2025-08-12 22:24:20 +0530

    Corrected the api

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,13 +1,14 @@
-import axios from 'axios';
-import type { Timer } from './types';
-
+import axios from "axios";
+import type { Timer } from "./types";
 const api = axios.create({
-  baseURL: import.meta.env.PROD
-    ? 'https://countdown-timer-app.onrender.com/api'
-    : '/api'
+  baseURL: "https://countdown-timer-app.onrender.com/api",
 });
 
-export const listTimers = (shop: string) => api.get<Timer[]>('/timers', { params: { shop } }).then(r => r.data);
-export const createTimer = (t: Timer) => api.post<Timer>('/timers', t).then(r => r.data);
-export const updateTimer = (id: string, t: Partial<Timer>) => api.put<Timer>(`/timers/${id}`, t).then(r => r.data);
-export const deleteTimer = (id: string) => api.delete(`/timers/${id}`).then(r => r.data);
+export const listTimers = (shop: string) =>
+  api.get<Timer[]>("/timers", { params: { shop } }).then((r) => r.data);
+export const createTimer = (t: Timer) =>
+  api.post<Timer>("/timers", t).then((r) => r.data);
+export const updateTimer = (id: string, t: Partial<Timer>) =>
+  api.put<Timer>(`/timers/${id}`, t).then((r) => r.data);
+export const deleteTimer = (id: string) =>
+  api.delete(`/timers/${id}`).then((r) => r.data);
